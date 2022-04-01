3a895ed26d50db51a2cb1009160680269e981a75
Katya Pushkareva <katieperca@yandex.ru>
2022-04-02 01:32:45 +0300

    реализовать попап

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('#name');
+let jobInput = formElement.querySelector('#job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let saveButton = formElement.querySelector('.form__save-button');
+
+editButton.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+});
+
+closeButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+  nameInput.value = '';
+  jobInput.value = '';
+});
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
